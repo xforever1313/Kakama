5ccc379219dbe29ee6620494bc2ccb4351c5149a
Seth Hendrick <seth@shendrick.net>
2023-07-28 21:38:52 -0400

    Initial project structure.

---
+++ src/Kakama.Web/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿//
+// Kakama - An ActivityPub Bot Framework
+// Copyright (C) 2023 Seth Hendrick
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using System.Diagnostics;
+using Kakama.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kakama.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController( ILogger<HomeController> logger )
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache( Duration = 0, Location = ResponseCacheLocation.None, NoStore = true )]
+        public IActionResult Error()
+        {
+            return View( new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier } );
+        }
+    }
+}
