6c7519978926f1c4cafa02ca355ed324f0f54701
Seth Hendrick <seth@shendrick.net>
2023-12-03 17:23:22 -0500

    Added the ability to test the CLI via unit tests.
    Test the namespace add functionality via the CLI.

---
+++ src/Kakama.Tests/Cli/Commands/Namespace/NamespaceAddCommandTests.cs
@@ -0,0 +1,124 @@
+﻿//
+// Kakama - An ActivityPub Bot Framework
+// Copyright (C) 2023 Seth Hendrick
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+namespace Kakama.Tests.Cli.Commands.Namespace
+{
+    [TestClass]
+    [DoNotParallelize] // <- Makes sure we don't share the .db file on multiple threads.
+    public sealed class NamespaceAddCommandTests
+    {
+        // ---------------- Fields ----------------
+
+        private KakamaCliHarness? uut;
+
+        // ---------------- Setup / Teardown ----------------
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            this.uut = new KakamaCliHarness( "namespaceclitests.db", "namespacecli.env" );
+            this.uut.PerformTestSetup();
+        }
+
+        [TestCleanup]
+        public void TestTeardown()
+        {
+            this.uut?.PerformTestTearDown();
+        }
+
+        // ---------------- Properties ----------------
+
+        public KakamaCliHarness Uut
+        {
+            get
+            {
+                Assert.IsNotNull( this.uut );
+                return this.uut;
+            }
+        }
+
+        // ---------------- Tests ----------------
+
+        [TestMethod]
+        public void AddWithOptionalsNotSpecified()
+        {
+            // Setup
+            const string expectedName = "My Name";
+            var args = new string[]
+            {
+                "namespace",
+                "add",
+                $"--name={expectedName}"
+            };
+
+            var expectedNs = new Kakama.Api.Models.Namespace
+            {
+                Id = 1,
+                BaseUrl = null,
+                Name = expectedName,
+                Slug = "my-name"
+            };
+
+            // Act
+            int exitCode = this.Uut.RunArgumentsWithDefaultGlobalSettings( args );
+            Assert.AreEqual( 0, exitCode );
+
+            // Check
+            IEnumerable<Kakama.Api.Models.Namespace> allNamespaces = this.Uut.ApiHarness.NamespaceManager.GetAllNamespaces();
+            Assert.AreEqual( 1, allNamespaces.Count() );
+
+            Assert.AreEqual( expectedNs, allNamespaces.First() );
+        }
+
+        [TestMethod]
+        public void AddWithOptionalsSpecified()
+        {
+            // Setup
+            const string expectedName = "My Name";
+            const string expectedSlug = "my-slug";
+            Uri expectedBaseUri = new Uri( "https://shendrick.net" );
+
+            var args = new string[]
+            {
+                "namespace",
+                "add",
+                $"--name={expectedName}",
+                $"--slug={expectedSlug}",
+                $"--base_uri={expectedBaseUri}"
+            };
+
+            var expectedNs = new Kakama.Api.Models.Namespace
+            {
+                Id = 1,
+                BaseUrl = expectedBaseUri,
+                Name = expectedName,
+                Slug = expectedSlug
+            };
+
+            // Act
+            int exitCode = this.Uut.RunArgumentsWithDefaultGlobalSettings( args );
+            Assert.AreEqual( 0, exitCode );
+
+            // Check
+            IEnumerable<Kakama.Api.Models.Namespace> allNamespaces = this.Uut.ApiHarness.NamespaceManager.GetAllNamespaces();
+            Assert.AreEqual( 1, allNamespaces.Count() );
+
+            Assert.AreEqual( expectedNs, allNamespaces.First() );
+        }
+    }
+}
---
+++ src/Kakama.Tests/KakamaCliHarness.cs
@@ -0,0 +1,100 @@
+﻿//
+// Kakama - An ActivityPub Bot Framework
+// Copyright (C) 2023 Seth Hendrick
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Kakama.Cli.Commands;
+using SethCS.Extensions;
+
+namespace Kakama.Tests
+{
+    public class KakamaCliHarness
+    {
+        // ---------------- Constructor ----------------
+
+        public KakamaCliHarness( string dbFileName, string dotEnvFileName )
+        {
+            this.ApiHarness = new KakamaApiHarness( dbFileName );
+            this.DotEnvFileLocation = new FileInfo(
+                Path.Combine(
+                    Path.GetDirectoryName( typeof( KakamaCliHarness ).Assembly.Location ) ?? "",
+                    dotEnvFileName
+                )
+            );
+        }
+
+        // ---------------- Properties ----------------
+
+        public KakamaApiHarness ApiHarness { get; private set; }
+
+        public FileInfo DotEnvFileLocation { get; private set; }
+
+        // ---------------- Setup / Teardown ----------------
+
+        public void PerformTestSetup()
+        {
+            if( this.DotEnvFileLocation.Exists )
+            {
+                File.Delete( this.DotEnvFileLocation.FullName );
+            }
+            File.WriteAllText(
+                this.DotEnvFileLocation.FullName,
+$@"DATABASE_ENGINE=Sqlite
+DATABASE_SQLITE_FILE={this.ApiHarness.DbFileLocation.FullName}
+
+# Must be false since otherwise this doesn't get cleaned up
+# in unit test land for some reason.
+DATABASE_SQLITE_POOL_CONNECTION=false
+"
+            );
+
+            this.ApiHarness.PerformTestSetup();
+        }
+
+        public void PerformTestTearDown()
+        {
+            this.ApiHarness.PerformTestTearDown();
+
+            if( this.DotEnvFileLocation.Exists )
+            {
+                File.Delete( this.DotEnvFileLocation.FullName );
+            }
+        }
+
+        // ---------------- Functions ----------------
+
+        public int RunArgumentsWithDefaultGlobalSettings( string[] args )
+        {
+            var allArgs = new List<string>( args )
+            {
+                "env",
+                this.DotEnvFileLocation.FullName
+            };
+
+            return RunArguments( allArgs.ToArray() );
+        }
+
+        public int RunArguments( string[] args )
+        {
+            Console.WriteLine( "Invoking command line with the following args:" );
+            Console.WriteLine( args.ToListString( "  " ) );
+            Console.WriteLine();
+
+            var mainCommand = new MainCommand( Console.Out );
+            return mainCommand.Invoke( args );
+        }
+    }
+}
