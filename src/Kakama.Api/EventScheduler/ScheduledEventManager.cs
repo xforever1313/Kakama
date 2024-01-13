a2989f3e19c1de35a8a54f3bc2c91395453017d1
Seth Hendrick <seth@shendrick.net>
2024-01-13 11:46:02 -0500

    Forgot license header

--- src/Kakama.Api/EventScheduler/ScheduledEventManager.cs
+++ src/Kakama.Api/EventScheduler/ScheduledEventManager.cs
@@ -1,9 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
+﻿//
+// Kakama - An ActivityPub Bot Framework
+// Copyright (C) 2023 Seth Hendrick
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 using Quartz;
 using Quartz.Logging;
 using Serilog.Extensions.Logging;
