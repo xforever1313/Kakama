79b8cab6601e64366376c6e99ef1f3d1ad39b7ff
Seth Hendrick <seth@shendrick.net>
2024-01-13 16:53:00 -0500

    Remove extra usings.

--- src/Kakama.Tests/Api/Reporting/LogMessageCounterTests.cs
+++ src/Kakama.Tests/Api/Reporting/LogMessageCounterTests.cs
@@ -16,11 +16,6 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Kakama.Api.Reporting;
 using Serilog.Events;
 using Serilog.Parsing;
