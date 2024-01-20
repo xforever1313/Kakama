a2ff584c7d3333284c806fbc75236ab0edc3e0da
Seth Hendrick <seth@shendrick.net>
2024-01-20 16:11:11 -0500

    First pass at adding profile json

--- src/Kakama.Api/Models/Profile.cs
+++ src/Kakama.Api/Models/Profile.cs
@@ -18,7 +18,13 @@
 
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using KristofferStrube.ActivityStreams;
+using KristofferStrube.ActivityStreams.JsonLD;
 using SethCS.Exceptions;
+
+using Service = KristofferStrube.ActivityStreams.Service;
 
 namespace Kakama.Api.Models
 {
@@ -123,6 +129,233 @@ namespace Kakama.Api.Models
             {
                 throw new ListedValidationException( "Errors when validating profile", errors );
             }
+        }
+
+        public static Service ToActivityPubJason(
+            this Profile profile,
+            Namespace ns,
+            RsaKey rsaKey,
+            IEnumerable<ProfileMetaData> profileMetaData,
+            string baseSiteUrl
+        )
+        {
+            if( profile.Slug is null )
+            {
+                throw new ArgumentException(
+                    "Slug can not be null in the profile.",
+                    nameof( profile )
+                );
+            }
+            else if( profile.NamespaceId != ns.Id )
+            {
+                throw new ArgumentException(
+                    $"Passed in namespace's ID of {ns.Id} does not match profile's namespace ID of {profile.NamespaceId}",
+                    nameof( ns )
+                );
+            }
+            else if( ns.Slug is null )
+            {
+                throw new ArgumentException(
+                    "Slug can not be null in the namespace.",
+                    nameof( ns )  
+                );
+            }
+            else if( rsaKey.Id != profile.RsaKeyId )
+            {
+                throw new ArgumentException(
+                    $"Passed in RSA Key's ID of {rsaKey.Id} does not match profile's RSA Key ID of {profile.RsaKeyId}",
+                    nameof( rsaKey )
+                );
+            }
+            else if( profileMetaData.Any( p => p.ProfileId != profile.Id ) )
+            {
+                throw new ArgumentException(
+                    $"At least one passed in piece of profile metadata's ID does ot match the profile ID of {profile.Id}",
+                    nameof( profileMetaData )
+                );
+            }
+
+            // The URL is http://website.com/namespace/profile/
+            // Things under profile include the stuff required for ActivityPub
+            // (e.g. the json files).
+            // index.html under the profile folder will be the human-readable
+            // section of the profile.
+            string profileFolderUrl = $"{baseSiteUrl}/{ns.Slug}/{profile.Slug}";
+
+            // Activity Pub URLs
+            string profileJsonUrl = $"{profileFolderUrl}/profile.json";
+            string outboxUrl = $"{profileFolderUrl}/outbox.json";
+            string inboxUrl = $"{profileFolderUrl}/inbox.json";
+            string followingUrl = $"{profileFolderUrl}/following.json";
+            string followersUrl = $"{profileFolderUrl}/followers.json";
+
+            // Human Readable URLs
+            string profileUrl = $"{profileFolderUrl}/index.html";
+
+            List<IObjectOrLink>? metadata;
+            if( profileMetaData.Any() == false )
+            {
+                metadata = null;
+            }
+            else
+            {
+                metadata = new List<IObjectOrLink>();
+                foreach( ProfileMetaData data in profileMetaData )
+                {
+                    metadata.Add(
+                        new PropertyValue
+                        {
+                            Name = new string[] { data.Name },
+                            Type = new string[] { "PropertyValue" },
+                            Value = data.Value
+                        }
+                    );
+                }
+            }
+
+            var key = new ProfilePublicKey
+            {
+                // ID Must match the Profile's ID.
+                Id = $"{profile.Id}#main-key",
+                Owner = profile.Id.ToString(),
+                PublicKeyPem = rsaKey.PublicKeyPem
+            };
+
+            var extensionData = new Dictionary<string, JsonElement>
+            {
+                ["discoverable"] = JsonSerializer.SerializeToElement( true ),
+                ["manuallyApprovesFollowers"] = JsonSerializer.SerializeToElement( false ),
+                ["@context"] = JsonSerializer.SerializeToElement(
+                    new object[]
+                    {
+                        "https://www.w3.org/ns/activitystreams",
+                        "https://w3id.org/security/v1",
+                        new PropertyValueSchema()
+                        {
+                            PropertyValue = "schema:PropertyValue",
+                            Value = "schema:value"
+                        }
+                    }
+                ),
+                ["publicKey"] = JsonSerializer.SerializeToElement( key )
+            };
+        
+            string[]? summary;
+            if( profile.Description is null )
+            {
+                summary = null;
+            }
+            else
+            {
+                summary = new string[]
+                {
+                    profile.Description
+                };
+            }
+
+            Image[]? icon;
+            if( profile.ImageUrl is null )
+            {
+                icon = null;
+            }
+            else
+            {
+                icon = new Image[]
+                {
+                    new Image
+                    {
+                        Type = new string[]{ "Image" },
+                        MediaType = $"image/{Path.GetExtension( profile.ImageUrl.ToString() ).TrimStart( '.' )}",
+                        Url = new Link[]
+                        {
+                            new Link
+                            {
+                                Href = profile.ImageUrl
+                            }
+                        }
+                    }
+                };
+            }
+
+            var service = new Service
+            {
+                // ID must be the same as the URL to the profile JSON (its a self-reference).
+                Id = profileJsonUrl,
+                Type = new string[]{ "Service" },
+
+                // URL in this context is the link to the human-readable URL,
+                // not the profile json.
+                Url = new Link[]
+                { 
+                    new Link
+                    {
+                        Href = new Uri( profileUrl )
+                    }
+                },
+                Inbox = new Link
+                {
+                    Href = new Uri( inboxUrl )  
+                },
+                Outbox = new Link
+                {
+                    Href = new Uri( outboxUrl )
+                },
+                Following = new Link
+                {
+                    Href = new Uri( followingUrl )
+                },
+                Followers = new Link
+                {
+                    Href = new Uri( followersUrl )
+                },
+
+                // Profile Info
+                PreferredUsername = profile.Slug,
+                Name = new string[]
+                {
+                    profile.Name
+                },
+                Summary = summary,
+                Published = DateTime.Now, // TODO: Not this.
+                Icon = icon,
+                Attachment = metadata,
+                ExtensionData = extensionData
+            };
+
+            return service;
+        }
+
+        private class PropertyValue : KristofferStrube.ActivityStreams.Object
+        {
+            [JsonPropertyName("value")]
+            [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+            public string? Value { get; set; }
+        }
+
+        private class PropertyValueSchema : ITermDefinition
+        {
+            [JsonPropertyName( "PropertyValue" )]
+            [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+            public string? PropertyValue { get; set; }
+
+            [JsonPropertyName( "value" )]
+            [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+            public string? Value { get; set; }
+        }
+
+        private record class ProfilePublicKey
+        {
+            [JsonPropertyName( "id" )]
+            [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+            public string? Id { get; init; }
+
+            [JsonPropertyName( "owner" )]
+            [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+            public string? Owner { get; init; }
+
+            [JsonPropertyName( "publicKeyPem" )]
+            [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+            public string? PublicKeyPem { get; init; }
         }
     }
 }
